0e8e0e6ff74b4464aaece3e41901918ed5fbf6d5
adrien <adrien.constan@gmail.com>
2022-07-06 20:56:40 +0200

    auto-commit

--- part_3/install_packages.sh
+++ part_3/install_packages.sh
@@ -14,7 +14,7 @@ snap install kubectl --classic
 echo -e "$RED Installing K3D ... $WHITE"
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
-echo -e "$RED Installing ARGOCD ... $WHITE"
-sudo apt-get install argocd -y
+#echo -e "$RED Installing ARGOCD ... $WHITE"
+#sudo apt-get install argocd -y
 
 echo -e "$GREEN End of script ... $WHITE\n"
