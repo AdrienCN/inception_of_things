73f0e0e14f793d977f6b2cad93e069dcaa3763e2
adrien <adrien.constan@gmail.com>
2022-07-08 17:45:28 +0200

    auto-commit

--- part_3/script/deploy_project.sh
+++ part_3/script/deploy_project.sh
@@ -15,5 +15,8 @@ echo "creating the ingress"
 sudo kubectl apply -n argocd -f ../conf/argo_install.yaml
 sleep 3
 
+sudo kubectl apply -n argocd -f ../conf/project.yaml
+sleep 3
+
 echo "creating the wil app"
 sudo kubectl apply -f ../conf/application.yaml -n argocd
