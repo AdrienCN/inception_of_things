14cb56204de7cf1266dfc147466b3c77d7c6f95c
adrien <adrien.constan@gmail.com>
2022-07-13 14:38:14 +0200

    auto-commit

--- bonus/scripts/bonus.sh
+++ bonus/scripts/bonus.sh
@@ -1,28 +1,19 @@
 #!/bin/bash
 
-HOST_IP=192.168.42.42
+sudo k3d cluster edit adconsta --port-add 8889:30889@loadbalancer
 
-k3d cluster edit part3 --port-add 8889:30889@loadbalancer
+sudo kubectl create namespace gitlab
+echo -e "\n----------------Bonus-----------------"
+echo -e "\ninstalling project Bonus to argocd..."
 
-kubectl create namespace gitlab
-echo "\n----------------Bonus-----------------"
-echo "\ninstalling project Bonus to argocd..."
-
-if grep -q HOST_IP ../confs/project.yamll then
-    sed -i 's/HOST_IP/'"$HOST_IP"'/g' ../confs/project.yaml
-fi
-
-kubectl apply -f ../confs/project.yaml -n argocd
-echo "\ninsatlled progect to argocd"
+sudo kubectl apply -f ../confs/project.yaml -n argocd
+echo -e "\ninsatlled progect to argocd"
 sleep 3
 
-echo "\ninstalling application Bonus to argocd..."
-if grep -q HOST_IP ../confs/application.yamll then
-    sed -i 's/HOST_IP/'"$HOST_IP"'/g' ../confs/application.yamll
-fi
+echo -e "\ninstalling application Bonus to argocd..."
 
-kubectl apply -f ../confs/application.yaml -n argocd
-echo "\ninsatlled application to argocd"
+sudo kubectl apply -f ../confs/application.yaml -n argocd
+echo -e "\ninsatlled application to argocd"
 sleep 3
 
 echo "Done for Bonus !"
